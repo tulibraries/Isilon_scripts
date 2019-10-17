8be5f67bb69553d17d31a431c51f90b7b23ca705
Tim Bieniosek <tug76662@temple.edu>
2019-10-17 16:31:16 -0400

    Version one of the Isilon copy script

--- isilon_copy.sh
+++ isilon_copy.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
-
-# curl "https://155.247.166.127:8080/platform/?describe&list&all" -k --basic --user username:password --verbose
-# curl "https://155.247.166.127:8080/platform/1/protocols/nfs/exports" -k --basic --user username:password --verbose
-# curl "https://155.247.166.127:8080/platform/1/protocols/nfs/exports" -k -X PUT --basic --user username:password --verbose
-# curl "https://155.247.166.127:8080/namespace/ifs/data/libraryfs/test-access/testapi" -k --user username:password 
-# curl "https://155.247.166.127:8080/namespace/ifs/data/libraryfs/test-repository/?detail=name,type" -k --user username:password 
-# curl "https://155.247.166.127:8080/namespace/ifs/data/libraryfs/test-access/testapi" -k -X PUT --basic --user username:password --verbose -H 'x-isi-ifs-target-type: container' -H 'x-isi-ifs-copy-source: /namespace/ifs/data/libraryfs/test-repository'
-# curl "https://155.247.166.127:8080/namespace/ifs/data/libraryfs/test-access/testapi?merge=true&continue=true" -k -X PUT --basic --user username:password -H 'x-isi-ifs-target-type: container' -H 'x-isi-ifs-copy-source: /namespace/ifs/data/libraryfs/test-repository'
-
-
 NOW=$(date +"%F_%H-%M-%S")
 LOGFILE="isilonapi_copy_log-$NOW.log"
 
 exec > $LOGFILE                                                                      
 exec 2>&1
 
-curl "https://155.247.166.127:8080/namespace/ifs/data/libraryfs/test-access/testapi?merge=true&continue=true" --verbose -k -X PUT --basic --user username:password -H 'x-isi-ifs-target-type: container' -H 'x-isi-ifs-copy-source: /namespace/ifs/data/libraryfs/media-transfer/Move_to_Media_Transfer/Audio'
+# copy a directory:
+curl "https://155.247.166.127:8080/namespace/ifs/data/libraryfs/media-repository/Audio/?merge=true&continue=true&overwrite=false" --verbose -k -X PUT --basic --user username:password -H 'x-isi-ifs-target-type: container' -H 'x-isi-ifs-copy-source: /namespace/ifs/data/libraryfs/media-transfer/Move_to_Media_Transfer/Audio'
+curl "https://155.247.166.127:8080/namespace/ifs/data/libraryfs/media-repository/Video/?merge=true&continue=true&overwrite=false" --verbose -k -X PUT --basic --user username:password -H 'x-isi-ifs-target-type: container' -H 'x-isi-ifs-copy-source: /namespace/ifs/data/libraryfs/media-transfer/Move_to_Media_Transfer/Video'
+
+#list target when complete
+echo " "
+echo "Isilon API actions finished. Recursive directory listing of target follows:"
+echo " "
+echo "Audio:"
+curl "https://155.247.166.127:8080/namespace/ifs/data/libraryfs/media-repository/Audio/?limit=-1&detail=size,last_modified" -k -X GET --basic --user username:password -H 'x-isi-ifs-target-type: container'
+echo " "
+echo "Video:"
+curl "https://155.247.166.127:8080/namespace/ifs/data/libraryfs/media-repository/Video?limit=-1&detail=size,last_modified" -k -X GET --basic --user username:password -H 'x-isi-ifs-target-type: container'
+
+ls -R /media/ifs/data/libraryfs/media-repository/
